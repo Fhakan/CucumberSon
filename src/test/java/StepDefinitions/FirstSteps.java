703c9a7f546656876b399c5a1eed26b91d823a50
HZB <fehimhakan@gmail.com>
2022-09-01 11:40:52 +0300

    hakan yaptı 2

--- src/test/java/StepDefinitions/FirstSteps.java
+++ src/test/java/StepDefinitions/FirstSteps.java
@@ -3,5 +3,6 @@ package StepDefinitions;
 public class FirstSteps {
     public static void main(String[] args) {
         System.out.println("merhaba hüseyin");
+        System.out.println("merhaba hakan");
     }
 }
