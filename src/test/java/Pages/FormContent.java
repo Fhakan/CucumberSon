ce2eeb4550adf2623ab72dd944e3f536b7f9fbcf
Fhakan <94861100+Fhakan@users.noreply.github.com>
2022-09-12 22:17:05 +0300

    Update FormContent.java

--- src/test/java/Pages/FormContent.java
+++ src/test/java/Pages/FormContent.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class FormContent extends Parent {
+public class FormContent extends Parent{
 
     public FormContent() {
         PageFactory.initElements(GWD.getDriver(), this);
