39bdc596dd093d82566e4b1fecc5088829949b05
Fhakan <94861100+Fhakan@users.noreply.github.com>
2022-09-12 22:18:23 +0300

    Update FormContent.java

--- src/test/java/Pages/FormContent.java
+++ src/test/java/Pages/FormContent.java
@@ -14,21 +14,21 @@ public class FormContent extends Parent{
 
 
 
-//    @FindBy(xpath = "(//mat-option[@role='option']/span)[1]")
-//    private WebElement gradeLevel2;
+    @FindBy(xpath = "(//mat-option[@role='option']/span)[1]")
+    private WebElement gradeLevel2;
 
     WebElement myElement;
 
     public void findAndClick(String strElement) {  // 2.aşama
         // burda string isimden weblemente ulaşıcam
-//         switch(strElement)
+         switch(strElement)
         {
 
 
-//            case "academicPeriod" : myElement =academicPeriod; break;
-//            case "academicPeriod1" : myElement =academicPeriod1; break;
-//            case "gradeLevel" : myElement =gradeLevel; break;
-//            case "gradeLevel2" : myElement =gradeLevel2; break;
+            case "academicPeriod" : myElement =academicPeriod; break;
+            case "academicPeriod1" : myElement =academicPeriod1; break;
+            case "gradeLevel" : myElement =gradeLevel; break;
+            case "gradeLevel2" : myElement =gradeLevel2; break;
         }
 
     clickFunction(myElement);
